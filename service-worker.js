5dd7eedc9fdbf3c1c7f8fb8cd4be05205bfad110
Joseildo M S Filho <filhojoseildo@gmail.com>
2024-05-11 14:25:16 -0300

    chore: removes all the old fixed tabs

--- service-worker.js
+++ service-worker.js
@@ -11,3 +11,38 @@ chrome.tabs.onCreated.addListener(async (tab) => {
         chrome.tabs.remove(tabId)
     }
 })
+
+const deleteOldFixedAlarmName = 'delete-old-fixed'
+chrome.runtime.onInstalled.addListener(async ({ reason }) => {
+    if (reason !== 'install') {
+        return;
+    }
+
+    await chrome.alarms.create(deleteOldFixedAlarmName, {
+        delayInMinutes: 1,
+        periodInMinutes: 1
+    })
+})
+
+chrome.alarms.onAlarm.addListener(async ({ name }) => {
+    if(name !== deleteOldFixedAlarmName) {
+        return;
+    }
+
+    const fixedTabs = await chrome.tabs.query({
+        pinned: true,
+    })
+
+    const now = new Date();
+    now.setDate(now.getDate() - 7)
+    for(const tab of fixedTabs) {
+        const { lastAccessed } = tab
+        if(lastAccessed) {
+            const lastAccessedDate = new Date(lastAccessed)
+            if(lastAccessedDate < now) {
+                const tabId = tab.id
+                chrome.tabs.remove(tabId)
+            }
+        }
+    }
+})
